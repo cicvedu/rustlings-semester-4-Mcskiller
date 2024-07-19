a669d36a31c82b5186f74bfc9c2d516e6c2f87df
lebenito <i@lebenito.net>
2024-07-19 15:19:57 +0800

    🟢[100/100] completed update

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,9 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn longest(x: &str, y: &str) -> &str {
+
+fn longest<'v>(x: &'v str, y: &'v str) -> &'v str {
     if x.len() > y.len() {
         x
     } else {
